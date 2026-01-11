9e84b18e0f10600fe0cd1e70a131e23fa38a9354
darineabdelmotalib <darine.motalib@gmail.com>
2026-01-11 14:30:21 -0800

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
+import "./Header.scss";
+
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 function Header() {
     return (
-        <>header</>
-    )
+        <header className="header">
+            <Link to="/" className="header__title">
+                PewPew Sherif
+            </Link>
+        </header>
+    );
 }
 
 export default Header;
